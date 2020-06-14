97b10d186f5e4910b3f1e244f96970636d17686d
Andres B <andresblasiche@gmail.com>
2020-06-14 20:17:14 -0300

    token refresh

--- jwt/token.js
+++ jwt/token.js
@@ -1,16 +1,37 @@
 const jwt = require('jsonwebtoken');
-const secreto = '4ndr35170';
+const secret = '4ndr35170';
 
 var token = {};
 
 token.codToken = function(userId) {
-	var key = jwt.sign({user: userId}, secreto);
-	return key;
+	try {
+		var key = jwt.sign({data: {user: userId}}, secret, {expiresIn: 60*15});
+		var keyRefresh = jwt.sign({data: {user: userId}}, secret, {expiresIn: 60*60*12});
+	} catch (error) {
+		console.error("Error al crear Token", error.message);
+	}
+	return {token: key, refresh: keyRefresh};
 };
 
 token.decToken = function(tkn) {
-	var decode = jwt.verify(tkn, secreto);
-	return decode;
+	try {
+		var payload = jwt.verify(tkn, secret);
+	} catch (error) {
+		console.error("Token invalido", error.message);
+	}
+	
+	return payload.data;
+};
+
+token.refresh = function(refTkn) {
+	try {
+		var payload = jwt.verify(refTkn, secret);
+		var newToken = jwt.sign({data: payload.data}, secret, {expiresIn: 60*15});
+	} catch (error) {
+		console.error("Token invalido", error.message);
+	}
+
+	return newToken;
 };
 
 module.exports = token;
