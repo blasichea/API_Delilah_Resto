18e980db1f4ed09422f1f724035edd32f8bf8c3d
Andres B <andresblasiche@gmail.com>
2020-06-14 18:01:45 -0300

    rutas para usuarios

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,9 +5,10 @@ const jwt = require('../jwt/token');
 router.use(function(req, res, next) {
 	if(!req.headers.token) {
 		res.status(401);
-		res.json("Se requiere Token");
+		return res.json("Se requiere Token");
 	}
-	var id = jwt.decToken(req.headers.token);
+	
+	var id = jwt.decToken(req.headers.token).user;
 	/* caduco?? */
 	db.user.findByPk(id)
 		.then(us => {
@@ -16,6 +17,41 @@ router.use(function(req, res, next) {
 	
 	next();
 });
+
+
+router.route('/info')
+	
+	.get(function(req, res) {
+		res.json({
+			user: req.user.user,
+			name: req.user.name,
+			email: req.user.email,
+			tel: req.user.tel,
+			address: req.user.address
+		});
+	})
+
+	.put(function(req, res) {
+		const { password } = req.body;
+
+		if (!password) {
+			res.status(400);
+			return res.json({mensaje: "Falta nuevo password"});
+		}
+
+		db.user.update({password: password}, {where:{id: req.user.id}})
+			.then(us => {
+				if (us === 0) {
+					res.status(400);
+					return res.json({mensaje: "No se actualizaron datos"});
+				}
+
+				res.json({mensaje: "Actualización exitosa"});
+			})
+			.catch(err => {
+				console.error("Error actualizando datos", err);
+			});
+	})
 
 
 router.get('/products', function(req, res) {
@@ -33,5 +69,7 @@ router.get('/products/:id', function(req, res) {
 		});
 });
 
+
+/* RUTAS PARA HACER PEDIDOS */
 
 module.exports = router;
