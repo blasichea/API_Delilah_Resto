e5c2001d40d6b04b264b17cdff2c7fb6d3fd5964
Andres B <andresblasiche@gmail.com>
2020-06-18 18:58:38 -0300

    archivo con variables de configuracion

---
+++ config.js
@@ -0,0 +1,18 @@
+var config = {
+	jwt: {
+		key: "4ndr35170"
+	},
+	db: {
+		host: "localhost",
+		port: "3306",
+		database: "delilah_resto",
+		user: "acamica",
+		pass: "AcamicA%232020",
+		dialect: "mysql"
+	},
+	server: {
+		port: "3000"
+	}
+}
+
+module.exports = config;
