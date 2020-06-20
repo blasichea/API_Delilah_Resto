d1be82baf1af23d739be0b3e57e1398e75af465a
Andres B <andresblasiche@gmail.com>
2020-06-20 03:15:28 -0300

    borre require que estaba demas

--- models/order.js
+++ models/order.js
@@ -1,5 +1,3 @@
-const { Sequelize } = require("sequelize/types");
-
 module.exports = (sequelize, DataTypes) => {
 	var state = ["Nuevo", "Preparando", "Enviado", "Entregado", "Cancelado"];
 	var pay = ["Cash", "Card", "Virtual", "Debit"];
@@ -12,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
 		},
 		time: {
 			type: DataTypes.DATE,
-			defaultValue: Sequelize.NOW
+			defaultValue: sequelize.NOW
 		},
 		detail: DataTypes.TEXT,
 		paying: {
